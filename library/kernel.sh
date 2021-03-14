ce0ccda59576db1b6c3cde1c07f41fc942cb6ed7
Marcel Hoffs <marcelhoffs@gmail.com>
2021-03-14 12:09:47 +0100

    kernel

--- library/kernel.sh
+++ library/kernel.sh
@@ -2,8 +2,10 @@
 if [ $1 == 'LTS' ]
   then
     # Install linux long term support kernel
+    echo ">> Installing Linux Long Term Support (LTS) kernel"
     pacman -S linux-lts linux-lts-headers linux-firmware
   else
     # Install linux long term support kernel
+    echo ">> Installing Linux kernel"
     pacman -S linux linux-headers linux-firmware
 fi
