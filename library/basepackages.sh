42320be7ce66c4e94c1b7b99c261a32f1202148f
Marcel Hoffs <marcelhoffs@gmail.com>
2021-03-14 13:25:35 +0100

    user creation

--- library/basepackages.sh
+++ library/basepackages.sh
@@ -2,4 +2,4 @@
 
 # Install base packages
 echo ">> Installing base packages"
-pacman -S --noconfirm grub efibootmgr dosfstools openssh os-prober mtools reflector networkmanager wpa_supplicant open-vm-tools
+pacman -S --noconfirm grub efibootmgr dosfstools openssh os-prober mtools reflector networkmanager wpa_supplicant open-vm-tools git curl wget htop neofetch
--- library/createuser.sh
+++ library/createuser.sh
@@ -7,6 +7,14 @@ if [ $# -eq 2 ]
     useradd -m -g users -s /bin/bash $1
     echo $1:$2 | chpasswd
     echo "$1 ALL=(ALL) ALL" >> /etc/sudoers.d/$1
+
+    # Create alias for ll
+    echo 'alias ll=''ls -l''' >> /home/$1/.bashrc
+
+    # Modify .bash_profile
+    echo '# Clear screen and run neofetch' >> /home/$1/.bash_profile
+    echo 'clear' >> /home/$1/.bash_profile
+    echo 'neofetch' >> /home/$1/.bash_profile
   else
     echo "Provide a username and password."
     echo "Usage: createuser.sh <username> <password>"
