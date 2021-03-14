5acde263ab8680b6d21e09eac091d3c45445f076
Marcel Hoffs <marcelhoffs@gmail.com>
2021-03-14 11:38:54 +0100

    commit

--- library/hostname.sh
+++ library/hostname.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 if [ $# -eq 1 ]
   then
+    # Create hostname (/etc/hostname)
     echo "Setting hostname to: $1"
     echo $1 > /etc/hostname
+    
+    # Create hosts file (/etc/hosts)
+    echo '127.0.0.1 localhost' >> /etc/hosts
+    echo '::1 localhost' >> /etc/hosts
+    echo '127.0.1.1 '$1'.localdomain '$1 >> /etc/hosts
   else
     echo "Provide a hostname."
     echo "Usage: hostname.sh <hostname>"
