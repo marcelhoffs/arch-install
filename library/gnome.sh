c76361e9a2c31b6b19795160116458fea8d0950b
Marcel Hoffs <marcelhoffs@gmail.com>
2021-03-14 17:47:34 +0100

    fix

--- library/gnome.sh
+++ library/gnome.sh
@@ -7,7 +7,7 @@ MINIMAL=${MINIMAL^^}
 echo ">> Installing GNOME"
 pacman -S --noconfirm gdm gnome gnome-tweak-tool gnome-bluetooth chrome-gnome-shell gnome-software-packagekit-plugin
 
-if [ $MINIMAL == 'MINIMAL']
+if [ $MINIMAL == 'MINIMAL' ]
   then 
     # Minimalize GNOME
     pacman -Rsn --noconfirm gnome-books gnome-music gnome-maps gnome-contacts gnome-documents gnome-boxes gnome-calendar epiphany totem gnome-weather
