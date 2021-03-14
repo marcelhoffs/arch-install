e379fec8b2225fc83a28d7bf69d1e625a1ed2c19
Marcel Hoffs <marcelhoffs@gmail.com>
2021-03-14 13:10:42 +0100

    locale

--- library/locale.sh
+++ library/locale.sh
@@ -2,8 +2,8 @@
 
 # Set locale
 echo ">> Set locale"
-sed -i -e 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
-sed -i -e 's/# en_NL.UTF-8 UTF-8/en_NL.UTF-8 UTF-8/' /etc/locale.gen
-sed -i -e 's/# en_BE.UTF-8 UTF-8/en_BE.UTF-8 UTF-8/' /etc/locale.gen
+sed -i -e 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
+sed -i -e 's/#en_NL.UTF-8 UTF-8/en_NL.UTF-8 UTF-8/' /etc/locale.gen
+sed -i -e 's/#en_BE.UTF-8 UTF-8/en_BE.UTF-8 UTF-8/' /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
