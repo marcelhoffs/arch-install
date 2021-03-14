c87adbdd464fdf52af54207fef3567c0090a8ee2
Marcel Hoffs <marcelhoffs@gmail.com>
2021-03-14 18:52:27 +0100

    avahi

--- library/gnome.sh
+++ library/gnome.sh
@@ -5,13 +5,14 @@ MINIMAL=${MINIMAL^^}
 
 # Install GNOME
 echo ">> Installing GNOME"
-pacman -S --noconfirm gdm gnome gnome-tweak-tool gnome-bluetooth chrome-gnome-shell gnome-software-packagekit-plugin
+pacman -S --noconfirm gdm gnome gnome-tweak-tool gnome-bluetooth chrome-gnome-shell gnome-software-packagekit-plugin avahi
 
 if [ $MINIMAL == 'MINIMAL' ]
   then 
     # Minimalize GNOME
-    pacman -Rsn --noconfirm gnome-books gnome-music gnome-maps gnome-contacts gnome-documents gnome-boxes gnome-calendar epiphany totem gnome-weather
+    pacman -Rsn --noconfirm gnome-books gnome-music gnome-maps gnome-contacts gnome-documents gnome-boxes gnome-calendar epiphany totem gnome-weather gnome-photos
 fi 
 
 # Enable services
 systemctl enable gdm
+systemctl enable avahi-daemon
