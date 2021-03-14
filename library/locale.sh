82c4c38424bc4bf835e35984e822c15825e1d424
Marcel Hoffs <marcelhoffs@gmail.com>
2021-03-14 13:12:37 +0100

    locale dutch

--- library/locale.sh
+++ library/locale.sh
@@ -3,7 +3,7 @@
 # Set locale
 echo ">> Set locale"
 sed -i -e 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
-sed -i -e 's/#en_NL.UTF-8 UTF-8/en_NL.UTF-8 UTF-8/' /etc/locale.gen
-sed -i -e 's/#en_BE.UTF-8 UTF-8/en_BE.UTF-8 UTF-8/' /etc/locale.gen
+sed -i -e 's/#nl_NL.UTF-8 UTF-8/nl_NL.UTF-8 UTF-8/' /etc/locale.gen
+sed -i -e 's/#nl_BE.UTF-8 UTF-8/nl_BE.UTF-8 UTF-8/' /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
