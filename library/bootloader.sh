e85a6d39b524645e5897fbd80c4ab6cfd9fd5ff2
Marcel Hoffs <marcelhoffs@gmail.com>
2021-03-16 17:02:04 +0100

    bugfix

--- library/bootloader.sh
+++ library/bootloader.sh
@@ -7,12 +7,15 @@ INSTALL_TYPE=${INSTALL_TYPE^^}
 INSTALL_DEV=${INSTALL_DEV^^}
 
 # Install boot loader
-if [ INSTALL_TYPE = 'UEFI' ]
+if [ $INSTALL_TYPE = 'UEFI' ]
   then
     echo ">> Installing GRUB bootloader"
     grub-install --target=x86_64-efi --efi-directory=/boot/EFI --bootloader-id=grub_uefi --recheck
     grub-mkconfig -o /boot/grub/grub.cfg
-  else
+fi
+
+if [ $INSTALL_TYPE = 'BIOS' ]
+  then
     echo ">> Installing GRUB bootloader (BIOS)"
     grub-install --target=i386-pc $INSTALL_DEV
     grub-mkconfig -o /boot/grub/grub.cfg
