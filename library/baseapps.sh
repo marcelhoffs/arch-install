5557a64c6d4b7c4ab9a589c24967bf3f7bfe84e9
Marcel Hoffs <marcelhoffs@gmail.com>
2021-03-16 21:51:04 +0100

    removed samba

--- library/baseapps.sh
+++ library/baseapps.sh
@@ -4,9 +4,8 @@ NC='\e[0m'
 
 # Install base applications
 echo -e "${CYAN}>> Installing base applications${NC}"
-pacman -S --noconfirm --needed firefox samba git curl htop wget papirus-icon-theme flatpak cups avahi
+pacman -S --noconfirm --needed firefox git curl htop wget papirus-icon-theme flatpak cups avahi
 
 # Enable services
 systemctl enable cups
-systemctl enable samba
 systemctl enable avahi-daemon
