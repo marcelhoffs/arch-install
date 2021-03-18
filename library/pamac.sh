dd250e35655d47434fbf9b9a868d1cd985e810da
Marcel Hoffs <marcelhoffs@gmail.com>
2021-03-18 10:29:00 +0100

    add pamac script

---
+++ library/pamac.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+CYAN='\e[1;36m'
+NC='\e[0m'
+
+# Install Pamac
+echo ''
+echo -e "${CYAN}>> Install Pamac package manager${NC}"
+mkdir pamac
+cd pamac
+git clone https://aur.archlinux.org/pamac-aur.git .
+yes | makepkg -sic
