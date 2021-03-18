624ce6ff4d15c9859d34fed082b95cbb18597299
Marcel Hoffs <marcelhoffs@gmail.com>
2021-03-18 11:00:27 +0100

    tigervnc

--- library/tigervnc.sh
+++ library/tigervnc.sh
@@ -2,23 +2,29 @@
 CYAN='\e[1;36m'
 NC='\e[0m'
 
-# Install TigerVNC
-echo ''
-echo -e "${CYAN}>> Install TigerVNC package manager${NC}"
-sudo pacman -S --noconfirm tigervnc
-
-# Set VNC password
-echo ''
-echo -e "${CYAN}>> Provide VNC password${NC}"
-vncpasswd
-
-# Set user
-sudo echo ':1='"$USER" >> /etc/tigervnc/vncserver.users
-
-# Create config files
-echo 'session=gnome' > ~/.vnc/config
-echo 'geometry=1920x1080' >> ~/.vnc/config
-echo 'alwaysshared' >> ~/.vnc/config
-
-# Enable the service
-sudo systemctl enable vncserver@:1 --now
+# Run as regular user, not as root
+if [ "$EUID" -ne 0 ]; then
+  # Install TigerVNC
+  echo ''
+  echo -e "${CYAN}>> Install TigerVNC package manager${NC}"
+  sudo pacman -S --noconfirm tigervnc
+  
+  # Set VNC password
+  echo ''
+  echo -e "${CYAN}>> Provide VNC password${NC}"
+  vncpasswd
+  
+  # Set user
+  sudo echo ':1='"$USER" >> /etc/tigervnc/vncserver.users
+  
+  # Create config files
+  echo 'session=gnome' > ~/.vnc/config
+  echo 'geometry=1920x1080' >> ~/.vnc/config
+  echo 'alwaysshared' >> ~/.vnc/config
+  
+  # Enable the service
+  sudo systemctl enable vncserver@:1 --now 
+else
+  # Root
+  echo "Do not execute this script with root privileges"
+fi
