71912409a14e13f85131bd43bdf4cbb28589b1d0
Marcel Hoffs <marcelhoffs@gmail.com>
2021-03-18 10:36:17 +0100

    added yay

---
+++ library/yay.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+CYAN='\e[1;36m'
+NC='\e[0m'
+
+# Install Yay
+echo ''
+echo -e "${CYAN}>> Install Yay package manager${NC}"
+mkdir yay
+cd yay
+git clone https://aur.archlinux.org/yay.git .
+yes | makepkg -sic
