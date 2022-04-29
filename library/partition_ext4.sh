806e6a1042f81f9f228ed2deab10fa5b529a383a
Marcel Hoffs <marcelhoffs@gmail.com>
2022-04-29 10:12:24 +0200

    commit

--- library/partition_ext4.sh
+++ library/partition_ext4.sh
@@ -10,23 +10,23 @@ echo ''
 echo -e "${CYAN}"'>> Partitioning and formating drive: '"$DRIVE""${NC}"
 
 # Remove all partitions
-#sgdisk --zap-all /dev/sda
+sgdisk --zap-all /dev/sda
 
 # create EFI partition
-#sgdisk --new 1::+500M --typecode 1:ef00 --change-name 1:EFI /dev/sda
+sgdisk --new 1::+500M --typecode 1:ef00 --change-name 1:EFI "$DRIVE"1
 
 # create SWAP partition
-#sgdisk --new 2::+8G --typecode 2:8200 --change-name 2:SWAP /dev/sda
+sgdisk --new 2::+8G --typecode 2:8200 --change-name 2:SWAP "$DRIVE"2
 
 # create DATA parition
-#sgdisk --new 3:: --typcode 2:8300 --change-name 3:DATA /dev/sda
+sgdisk --new 3:: --typcode 2:8300 --change-name 3:DATA "$DRIVE"3
 
 # format EFI partition
-#mkfs.fat -F32 /dev/sda1
+mkfs.fat -F32 /dev/sda1
 
 # format SWAP partition
-#mkswap /dev/sda2
-#swapon /dev/sda2
+mkswap /dev/sda2
+swapon /dev/sda2
 
 # format DATA partition
-#mkfs.ext4 /dev/sda3
+mkfs.ext4 /dev/sda3
