c9cecc5c4db9155d9a275738a663c3c71c22e89f
Marcel Hoffs <marcelhoffs@gmail.com>
2022-04-29 10:48:56 +0200

    fix

--- library/partition_ext4.sh
+++ library/partition_ext4.sh
@@ -13,13 +13,13 @@ echo -e "${CYAN}"'>> Partitioning and formating drive: '"$DRIVE""${NC}"
 sgdisk --zap-all "$DRIVE"
 
 # create EFI partition
-sgdisk --new 1::+500M --typecode 1:ef00 --change-name 1:EFI "$DRIVE"1
+sgdisk --new 1::+500M --typecode 1:ef00 --change-name 1:EFI "$DRIVE"
 
 # create SWAP partition
-sgdisk --new 2::+8G --typecode 2:8200 --change-name 2:SWAP "$DRIVE"2
+sgdisk --new 2::+8G --typecode 2:8200 --change-name 2:SWAP "$DRIVE"
 
 # create DATA parition
-sgdisk --new 3:: --typcode 2:8300 --change-name 3:DATA "$DRIVE"3
+sgdisk --new 3:: --typcode 2:8300 --change-name 3:DATA "$DRIVE"
 
 # format EFI partition
 mkfs.fat -F32 "$DRIVE"1
