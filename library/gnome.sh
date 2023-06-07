d0637ff1c6ad1081be4a9e48e9ca3f9c4bb9c5d2
Marcel Hoffs <marcelhoffs@gmail.com>
2023-06-07 10:37:23 +0200

    fix for gnome

--- library/gnome.sh
+++ library/gnome.sh
@@ -5,7 +5,7 @@ NC='\e[0m'
 # Install GNOME
 echo ''
 echo -e "${CYAN}"'>> Installing GNOME'"${NC}"
-pacman -S --noconfirm gdm gnome gnome-tweak-tool gnome-bluetooth gnome-software-packagekit-plugin
+pacman -S --noconfirm gdm gnome gnome-tweak-tool gnome-bluetooth
 
 # Enable services
 systemctl enable gdm
