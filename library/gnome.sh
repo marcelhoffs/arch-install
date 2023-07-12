f200afea8f0e1ca8f2f1a4fb429b32612feb46c7
Marcel Hoffs <marcelhoffs@gmail.com>
2023-07-12 17:51:31 +0200

    added wireplumber and gst-plugin-pipewire

--- library/gnome.sh
+++ library/gnome.sh
@@ -5,7 +5,7 @@ NC='\e[0m'
 # Install GNOME
 echo ''
 echo -e "${CYAN}"'>> Installing GNOME'"${NC}"
-pacman -S --noconfirm gdm gnome gnome-tweak-tool gnome-bluetooth
+pacman -S --noconfirm gdm gnome gnome-tweak-tool gnome-bluetooth gst-plugin-pipewire wireplumber
 
 # Enable services
 systemctl enable gdm
