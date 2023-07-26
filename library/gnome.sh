47941c8fbe42b0681c91483a5689509feda80a0e
Marcel Hoffs <marcelhoffs@gmail.com>
2023-07-26 08:10:43 +0200

    added packages

--- library/gnome.sh
+++ library/gnome.sh
@@ -5,7 +5,7 @@ NC='\e[0m'
 # Install GNOME
 echo ''
 echo -e "${CYAN}"'>> Installing GNOME'"${NC}"
-pacman -S --noconfirm gdm gnome gnome-tweak-tool gnome-bluetooth gst-plugin-pipewire wireplumber power-profiles-daemon fwupd
+pacman -S --noconfirm gdm gnome gnome-tweak-tool gnome-bluetooth gst-plugin-pipewire wireplumber power-profiles-daemon gnome-firmware networkmanager-openconnect networkmanager-openvpn
 
 # Remove applications
 pacman -R --noconfirm epiphany gnome-music gnome-calendar
