fce5b3b4aa716d22a1582513c928729dddaebdd8
Marcel Hoffs <marcelhoffs@gmail.com>
2023-07-26 08:11:22 +0200

    remove grub

--- library/basepackages.sh
+++ library/basepackages.sh
@@ -5,4 +5,4 @@ NC='\e[0m'
 # Install base packages
 echo ''
 echo -e "${CYAN}"'>> Installing base packages'"${NC}"
-pacman -S --noconfirm grub efibootmgr dosfstools openssh os-prober mtools reflector networkmanager wpa_supplicant git curl wget htop neofetch bluez bluez-utils btrfs-progs archlinux-keyring zip unzip unrar fwupd
+pacman -S --noconfirm efibootmgr dosfstools openssh os-prober mtools reflector networkmanager wpa_supplicant git curl wget htop neofetch bluez bluez-utils btrfs-progs archlinux-keyring zip unzip unrar fwupd
