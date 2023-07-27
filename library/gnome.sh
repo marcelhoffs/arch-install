9c6c61c1d022560a840559a2d6ba4546a2d89169
Marcel Hoffs <marcelhoffs@gmail.com>
2023-07-27 23:21:43 +0200

    gnome

--- library/gnome.sh
+++ library/gnome.sh
@@ -10,5 +10,11 @@ pacman -S --noconfirm gdm gnome gnome-tweak-tool gnome-bluetooth gst-plugin-pipe
 # Remove applications
 pacman -R --noconfirm epiphany gnome-music gnome-calendar
 
+# Remove launcher icons
+rm /usr/share/applications/lstopo.desktop 
+rm /usr/share/applications/qv4l2.desktop 
+rm /usr/share/applications/qvidcap.desktop 
+rm /usr/share/applications/org.gnome.Tour.desktop
+
 # Enable services
 systemctl enable gdm
