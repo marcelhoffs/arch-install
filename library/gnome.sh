32d517e2f6dac30a9161757d87d9a43d5c933001
Marcel Hoffs <marcelhoffs@gmail.com>
2023-07-27 23:28:53 +0200

    gnome

--- library/gnome.sh
+++ library/gnome.sh
@@ -8,12 +8,14 @@ echo -e "${CYAN}"'>> Installing GNOME'"${NC}"
 pacman -S --noconfirm gdm gnome gnome-tweak-tool gnome-bluetooth gst-plugin-pipewire wireplumber power-profiles-daemon gnome-firmware networkmanager-openconnect networkmanager-openvpn
 
 # Remove applications
-pacman -R --noconfirm epiphany gnome-music gnome-calendar
+pacman -R --noconfirm epiphany gnome-music gnome-calendar gnome-photos
 
 # Remove launcher icons
 rm /usr/share/applications/lstopo.desktop 
 rm /usr/share/applications/qv4l2.desktop 
 rm /usr/share/applications/qvidcap.desktop 
+rm /usr/share/applications/stoken-gui.desktop 
+rm /usr/share/applications/stoken-gui-small.desktop 
 
 # Enable services
 systemctl enable gdm
