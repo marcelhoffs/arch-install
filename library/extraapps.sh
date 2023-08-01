3c2670a05ea7fdb0be49a1b7a02e490548118b30
Marcel Hoffs <marcelhoffs@gmail.com>
2023-08-01 08:53:24 +0200

    fix

--- library/extraapps.sh
+++ library/extraapps.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 CYAN='\e[1;36m'
 NC='\e[0m'
+CURRENTUSER=$(whoami)
 
 # Install extra packages
 echo ''
@@ -8,4 +9,4 @@ echo -e "${CYAN}"'>> Installing extra packages'"${NC}"
 pacman -S --noconfirm thunderbird gimp libreoffice-fresh libreoffice-fresh-nl bitwarden remmina gnome-boxes
 
 # Remove electron application launcher icon
-echo 'NoDisplay=true' > ~/.local/share/applications/electron24.desktop
+echo 'NoDisplay=true' > /home/"$CURRENTUSER"/.local/share/applications/electron24.desktop
--- library/gnome.sh
+++ library/gnome.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 CYAN='\e[1;36m'
 NC='\e[0m'
+CURRENTUSER=$(whoami)
 
 # Install GNOME
 echo ''
@@ -11,11 +12,11 @@ pacman -S --noconfirm gdm gnome gnome-tweak-tool gnome-bluetooth gst-plugin-pipe
 pacman -R --noconfirm epiphany gnome-music gnome-calendar gnome-photos
 
 # Remove launcher icons
-echo 'NoDisplay=true' > ~/.local/share/applications/lstopo.desktop
-echo 'NoDisplay=true' > ~/.local/share/applications/qv4l2.desktop
-echo 'NoDisplay=true' > ~/.local/share/applications/qvidcap.desktop
-echo 'NoDisplay=true' > ~/.local/share/applications/stoken-gui.desktop
-echo 'NoDisplay=true' > ~/.local/share/applications/stoken-gui-small.desktop
+echo 'NoDisplay=true' > /home/"$CURRENTUSER"/.local/share/applications/lstopo.desktop
+echo 'NoDisplay=true' > /home/"$CURRENTUSER"/.local/share/applications/qv4l2.desktop
+echo 'NoDisplay=true' > /home/"$CURRENTUSER"/.local/share/applications/qvidcap.desktop
+echo 'NoDisplay=true' > /home/"$CURRENTUSER"/.local/share/applications/stoken-gui.desktop
+echo 'NoDisplay=true' > /home/"$CURRENTUSER"/.local/share/applications/stoken-gui-small.desktop
 
 # Enable services
 systemctl enable gdm
