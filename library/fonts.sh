37549520a54630738eaa3ac7bd909c38412d9a66
Marcel Hoffs <marcelhoffs@gmail.com>
2023-08-01 08:50:39 +0200

    added font

--- library/fonts.sh
+++ library/fonts.sh
@@ -5,4 +5,4 @@ NC='\e[0m'
 # Install fonts
 echo ''
 echo -e "${CYAN}"'>> Installing fonts'"${NC}"
-pacman -S --noconfirm ttf-roboto ttf-fira-sans ttf-fira-mono ttf-fira-code noto-fonts
+pacman -S --noconfirm ttf-roboto ttf-fira-sans ttf-fira-mono ttf-fira-code noto-fonts ttf-liberation
