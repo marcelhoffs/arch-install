0d2c1beadd1fd0f3af672edaca60dc915bdfa0dd
Marcel Hoffs <marcelhoffs@gmail.com>
2023-10-12 22:02:50 +0200

    remove cheese

--- library/gnome.sh
+++ library/gnome.sh
@@ -8,7 +8,7 @@ echo -e "${CYAN}"'>> Installing GNOME'"${NC}"
 pacman -S --noconfirm gdm gnome gnome-tweak-tool gnome-bluetooth gst-plugin-pipewire wireplumber power-profiles-daemon gnome-firmware networkmanager-openconnect networkmanager-openvpn
 
 # Remove applications
-pacman -R --noconfirm epiphany gnome-music gnome-calendar gnome-photos
+pacman -R --noconfirm epiphany gnome-music gnome-calendar gnome-photos cheese
 
 # Enable services
 systemctl enable gdm
