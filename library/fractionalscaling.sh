9b638b2c1e36230310057314c7c988774c8f815b
Marcel Hoffs <marcel.hoffs@hrmc.be>
2024-01-29 09:52:57 +0100

    add fractional scaling

---
+++ library/fractionalscaling.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+CYAN='\e[1;36m'
+NC='\e[0m'
+
+if [ "$EUID" -ne 0 ]; then
+  # Not root
+  echo 'Please run with root privileges:'
+  echo './fractionalscaling.sh'
+else
+  # Enable fractional scaling
+  echo ''
+  echo -e "${CYAN}"'>> Enable fractional scaling'"${NC}"
+  
+  # Create config: /etc/dconf/profile/user
+  echo 'user-db:user' > /etc/dconf/profile/user
+  echo 'system-db:local' >> /etc/dconf/profile/user
+  
+  # Create config: /etc/dconf/db/local.d/00-hidpi
+  echo '[org/gnome/mutter]' > /etc/dconf/db/local.d/00-hidpi
+  echo 'experimental-features=['scale-monitor-framebuffer']' >> /etc/dconf/db/local.d/00-hidpi
+  
+  # Create config: /etc/dconf/db/locks/hidpi
+  echo '/org/gnome/mutter/experimental-features' > /etc/dconf/db/locks/hidpi
+  
+  # Update dconf
+  dconf update
+fi
