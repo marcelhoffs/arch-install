0df4968f04d52a16d014294bc6c8c7db257bdf47
Marcel Hoffs <marcel.hoffs@hrmc.be>
2024-01-29 10:10:03 +0100

    fix

--- library/fractionalscaling.sh
+++ library/fractionalscaling.sh
@@ -12,14 +12,17 @@ else
   echo -e "${CYAN}"'>> Enable fractional scaling'"${NC}"
   
   # Create config: /etc/dconf/profile/user
+  mkdir -p /etc/dconf/profile
   echo 'user-db:user' > /etc/dconf/profile/user
   echo 'system-db:local' >> /etc/dconf/profile/user
   
   # Create config: /etc/dconf/db/local.d/00-hidpi
+  mkdir -p /etc/dconf/db/local.d
   echo '[org/gnome/mutter]' > /etc/dconf/db/local.d/00-hidpi
   echo 'experimental-features=['scale-monitor-framebuffer']' >> /etc/dconf/db/local.d/00-hidpi
   
   # Create config: /etc/dconf/db/locks/hidpi
+  mkdir -p /etc/dconf/db/locks
   echo '/org/gnome/mutter/experimental-features' > /etc/dconf/db/locks/hidpi
   
   # Update dconf
